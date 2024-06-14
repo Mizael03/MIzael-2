90407cd7ffccf85767d8ec86a8609234adee9c00
Mizael03 <mizaelsilvadeoliveira03@gmail.com>
2024-06-13 21:01:06 -0300

    first commit

---
+++ porta.py
@@ -0,0 +1,12 @@
+
+O aluno mizael teve a primeira nota 3, a segunda nota 6 e a nota final 9 # type: ignore
+
+
+
+nome = 'Mizael'
+n1 = 3
+n2 = 6
+soma = n1 + n2
+
+
+print("o aluno" + nome + " teve a primeira nota" + n2  "e a nota final" )
